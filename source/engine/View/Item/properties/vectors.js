578dc59b21e634c093a3e6bc871ed0ffd105f5e1
Iain Reid <iainreid382@gmail.com>
2016-10-04 16:52:00 +0100

    [chore] Added documentation
    Documented the vector class.

--- source/engine/View/Item/properties/vectors.js
+++ source/engine/View/Item/properties/vectors.js
@@ -5,28 +5,62 @@ import { Platform } from '../../../Platform'
 
 function vectors (superclass) {
   return class Vectors extends superclass {
+    /**
+     * @description This method will return the horizontal value of the vector.
+     *
+     * @returns {Number} The vectors X axis value
+     */
     getVectorX () {
       return this.vectorX
     }
 
+    /**
+     * @description This method will set the horizontal value of the vector to the value supplied.
+     */
     setVectorX (x) {
       this.vectorX = x
     }
 
+    /**
+     * @description This method will return the vertical value of the vector.
+     *
+     * @returns {Number} The vectors Y axis value
+     */
     getVectorY () {
       return this.vectorY
     }
 
+    /**
+     * @description This method will set the vertical value of the vector to the value supplied.
+     */
     setVectorY (y) {
       this.vectorY = y
     }
 
+    /**
+     * @description This method will return the magnitude of the vector.
+     *
+     * @returns {Number} The magnitude of the vector
+     */
     getVectorMagnitude () {
-      return Math.sqrt(Math.pow(this.vectorX, 2), Math.pow(this.vectorY, 2))
+      return Platform.utils.pythagoras(this.vectorX, this.vectorY)
     }
 
+    /**
+     * @description This method will return the angle of direction for the vector.
+     *
+     * @returns {Number} The angle of direction of the vector
+     */
     getVectorDirection () {
-      Platform.utils.getAngleBetweenThreePoints(// todo)
+      const itemCenterCoordinates = super.getCenterCoordinates()
+
+      return Platform.utils.getAngleBetweenThreePoints({
+        dx: itemCenterCoordinates.dx,
+        dy: itemCenterCoordinates.dy - 1
+      }, itemCenterCoordinates, {
+        dx: itemCenterCoordinates.dx + this.vectorX,
+        dy: itemCenterCoordinates.dy + this.vectorY
+      })
     }
   }
 }
