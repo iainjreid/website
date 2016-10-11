55b587b805d2a90d75d3142862a9bead83e8d5e5
Iain Reid <iainreid382@gmail.com>
2016-10-11 22:30:50 +0100

    [chore] Collisions progress
    Added beginnings of tick function to embed continuous collision detection.

--- source/engine/View/Item/properties/collisions.js
+++ source/engine/View/Item/properties/collisions.js
@@ -1,11 +1,30 @@
 'use strict'
 
+// Dependencies
+import { Platform } from '../../../Platform'
+
+const updates = []
+
 function collisions (superclass) {
   return class Collisions extends superclass {
-    setCoordinates ({dx, dy}) {
-      super.setCoordinates({dx, dy})
+    setCoordinates (coordinates) {
+      updates.push({
+        tick: (coordinates) => {
+          super.setCoordinates(coordinates)
+        },
+        vector: coordinates
+      })
     }
   }
 }
+
+Platform.loop.add(() => {
+  let update
+
+  while (updates.length) {
+    update = updates.shift()
+    update.tick(update.vector)
+  }
+}, 100)
 
 export { collisions }
